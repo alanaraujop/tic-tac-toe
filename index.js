0a815777b2e9b483e5754c4f2db073168d645541
danailus <danailus@gmail.com>
2022-05-15 16:50:11 -0300

    Acrescentada função que verifica ganhador e se deu velha

--- index.js
+++ index.js
@@ -6,6 +6,39 @@ const players = {
 };
 let currentPlayer = players[1];
 
+const combinationsWinner = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function checkTieGame() {
+  const isDraw = boardData.every((item) => item !== emptyString);
+
+  if (isDraw) {
+    drawBoard(boardData);
+    console.log(`DEU VELHA!`);
+    process.exit();
+  }
+} 
+
+function checkWinner() {
+  combinationsWinner.forEach((winCondition) => {
+    const [a, b, c] = winCondition;
+
+    if (boardData[a] !== emptyString && boardData[a] === boardData[b] && boardData[a] === boardData[c]) {
+      drawBoard(boardData);
+      console.log(`Tivemos vencedor ${currentPlayer}`);
+      process.exit();
+    }
+  });
+}
+
 function drawBoard(positions) {
   console.log(`
     ${positions[0]} | ${positions[1]} | ${positions[2]}
@@ -19,13 +52,11 @@ function drawBoard(positions) {
 function showTutorial() {
   console.log(`
   ### Escolha a posiçao que deseja jogar baseado no tabuleiro abaixo ###
-
      0 | 1 | 2
     -----------
      3 | 4 | 5
     -----------
      6 | 7 | 8
-
   ######################################################################
   `);
 }
@@ -46,6 +77,8 @@ function setPlayerMovement(position) {
   if (isEmpty(position)) {
     boardData[position] = currentPlayer;
     changePlayer();
+    checkWinner();
+    checkTieGame();
   } else {
     console.log(`A posição ${position} já foi usada`);
   }
@@ -53,9 +86,14 @@ function setPlayerMovement(position) {
 
 // showTutorial();
 setPlayerMovement(0);
-setPlayerMovement(1);
-setPlayerMovement(2);
 setPlayerMovement(3);
+setPlayerMovement(8);
+setPlayerMovement(2);
 setPlayerMovement(4);
+setPlayerMovement(7);
+setPlayerMovement(1);
+setPlayerMovement(6);
+setPlayerMovement(5);
+
 
 drawBoard(boardData);
