f7b794a86ea92ea5b076a824c2f2fa41749645df
Maurício Azevedo <mauricio.a.neto@hotmail.com>
2022-05-15 21:38:09 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const readlineSync = require('readline-sync');
 const emptyString = '  ';
-const boardData = Array(9).fill(emptyString);
+let boardData = Array(9).fill(emptyString);
 const players = {
-  1: 'x',
-  2: 'o',
+  1: 'X',
+  2: 'O',
 };
+const scoreboard = {
+  "X": 0,
+  "O": 0
+}
+const winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 let currentPlayer = players[1];
+let currentPlayPosition;
+let choose = "";
+let firstPlayer;
 
 function drawBoard(positions) {
   console.log(`
@@ -45,17 +54,100 @@ function changePlayer() {
 function setPlayerMovement(position) {
   if (isEmpty(position)) {
     boardData[position] = currentPlayer;
-    changePlayer();
   } else {
     console.log(`A posição ${position} já foi usada`);
   }
 }
 
-// showTutorial();
-setPlayerMovement(0);
-setPlayerMovement(1);
-setPlayerMovement(2);
-setPlayerMovement(3);
-setPlayerMovement(4);
+function winnerCheck(winningCombinations, boardData) {
+  let filledPositions = 0;
 
-drawBoard(boardData);
+  for(array of winningCombinations) {
+    for(position of array) {
+      if(boardData[position] === currentPlayer) {
+        filledPositions++;
+      }else{
+        filledPositions = 0;
+        break;
+      }
+
+      if(filledPositions === 3){
+        return true;
+      }
+    }
+  }
+
+  return false;
+} 
+
+function isDraw(boardData) {
+  if(boardData.includes(emptyString)){
+    return false;
+  }
+
+  return true;
+}
+
+function cleanBoard() {
+  boardData = Array(9).fill(emptyString);
+}
+
+function countScore(currentPlayer) {
+  scoreboard[currentPlayer]++;
+}
+
+function showScore() {
+  console.log(`#####Placar#####
+  X: ${scoreboard['X']} pontos
+  O: ${scoreboard['O']} pontos
+################`);
+}
+
+function showWinner() {
+  if(scoreboard["X"] > scoreboard["O"]){
+    console.log("O jogador X é o vencedor!");
+  }else if(scoreboard["X"] < scoreboard["O"]){
+    console.log("O jogador O é o vencedor!");
+  }else{
+    console.log("Empatou! :/");
+  }
+}
+
+function round() {
+  cleanBoard(boardData);
+
+  firstPlayer = readlineSync.question("Quem vai começar jogando? 1 para O ou 2 para X: ");
+  currentPlayer = players[firstPlayer];
+
+  while(!winnerCheck(winningCombinations, boardData) && !isDraw(boardData)) {
+    changePlayer();
+
+    currentPlayPosition = readlineSync.question(`Digite a posicao (Vez de ${currentPlayer}):`);
+    setPlayerMovement(currentPlayPosition);
+
+    drawBoard(boardData);
+  }
+
+  if(isDraw(boardData) && !winnerCheck(winningCombinations, boardData)){
+    console.log("Deu velha!");
+  }else{
+    countScore(currentPlayer);
+    console.log(currentPlayer + " venceu!!");
+  }
+
+  showScore();
+}
+
+showTutorial();
+
+do{
+  round();
+
+  choose = readlineSync.question("Deseja continuar jogando? (y/n): ");
+}while(choose.toLowerCase() === "y");
+
+showWinner();
+
+
+
+
