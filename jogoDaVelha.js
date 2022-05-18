fc26205321657c31d41040fd5a4bebce01ce19b5
Victor Almeida <victorpy.1999@gmail.com>
2022-05-18 12:06:44 -0300

    Polindo código parte 2

--- jogoDaVelha.js
+++ jogoDaVelha.js
@@ -13,8 +13,8 @@ let currentPlayer = players[1];
 let currentNamePlayer = playerOne
 
 //Variáveis criadas para estabelecer um placar
-let scorePlayerOne = { nome: playerOne, score: 0 }
-let scorePlayerTwo = { nome: playerTwo, score: 0 }
+let logPlayerOne = { nome: playerOne, score: 0 }
+let logPlayerTwo = { nome: playerTwo, score: 0 }
 
 
 //Desenha o tabuleiro e um mapa para auxiliar nas jogadas
@@ -30,22 +30,12 @@ function drewBoard(positions) {
 
 //Faz a mudança automatica dos jogadores após efetuar uma jogada
 function changePlayer() {
-    if (currentPlayer === players[1]) {
-        return currentPlayer = players[2]
-    }
-    else {
-        return currentPlayer = players[1]
-    }
+    currentPlayer === players[1] ? currentPlayer = players[2] : currentPlayer = players[1]
 }
 
 //Faz a mudança do NOME dos jogadores após efetuar uma jogada
 function changePlayerName() {
-    if (currentNamePlayer === playerOne) {
-        return currentNamePlayer = playerTwo
-    }
-    else {
-        return currentNamePlayer = playerOne
-    }
+    currentNamePlayer === playerOne ? currentNamePlayer = playerTwo : currentNamePlayer = playerOne
 }
 
 //Função que executa a jogada no tabuleiro, verifica se há um vencedor ou se deu velha
@@ -59,14 +49,8 @@ function setPlayerMovement(position) {
         changePlayer()
     }
     else {
-        if (position >= 0 && position < 9) {
-            console.log(`A posição ${position} ja foi utilizada.`)
-        }
-        else {
-            console.log('A posição que você digitou é inválida.')
-        }
+        console.log(`A posição ${position} é inválida ou ja foi utilizada`)
     }
-
 }
 
 //Verifica se há um vencedor baseado nas possiveis combinações de vitória
@@ -95,12 +79,7 @@ function isEmpty(position) {
 
 //Verifica se deu velha
 function isOldLady(boardData) {
-    if (boardData.includes(' ')) {
-        return false
-    }
-    else if (isWinner(boardData) === false) {
-        return true
-    }
+    return !boardData.includes(emptyString)
 }
 
 //Reinicia o tabuleiro após o fim de uma rodada
@@ -114,8 +93,8 @@ function restartBoard(boardData) {
 //Mostra o placar atual
 function showScore() {
     console.log(`
-[ ${playerOne} ] =>   ${scorePlayerOne.score}  
-[ ${playerTwo} ] =>   ${scorePlayerTwo.score} 
+[ ${playerOne} ] =>   ${logPlayerOne.score}  
+[ ${playerTwo} ] =>   ${logPlayerTwo.score} 
     `)
 }
 
@@ -123,33 +102,13 @@ function showScore() {
 //Marca pontos iguais caso o resultado seja 'DEU VELHA'
 function scoreGame() {
     if (isWinner(boardData) === true) {
-        if (currentNamePlayer === playerOne) {
-            scorePlayerOne.score += 3
-        }
-        else if (currentNamePlayer === playerTwo) {
-            scorePlayerTwo.score += 3
-        }
-    }
-    if (isOldLady(boardData) === true && isWinner(boardData === false)) {
-        scorePlayerOne.score += 1
-        scorePlayerTwo.score += 1
-    }
-    showScore()
-
-}
-
-//Função que verifica se os jogadores querem continuar a jogar
-function continueGame() {
-    let optionContinue = prompt('Deseja jogar outra partida: [s / n]-> ')
-    while (optionContinue != 's' && optionContinue != 'n') {
-        optionContinue = prompt("Entrada inválida, digite apenas 's' ou 'n': ")
-    }
-    if (optionContinue === 's') {
-        return true
+        currentNamePlayer === playerOne ? logPlayerOne.score += 3 : logPlayerTwo.score += 3
     }
     else {
-        return false
+        logPlayerOne.score += 1
+        logPlayerTwo.score += 1
     }
+    showScore()
 }
 
 //Função que encerra o jogo
@@ -162,12 +121,23 @@ PLACAR FINAL:`)
 _______________________________________`)
 }
 
+//Função que verifica se os jogadores querem continuar a jogar
+function continueGame() {
+    let optionContinue = prompt('Deseja jogar outra partida: [s / n]-> ')
+    while (optionContinue != 's' && optionContinue != 'n') {
+        optionContinue = prompt("Entrada inválida, digite apenas 's' ou 'n': ")
+    }
+    optionContinue === 's' ? ticTacToe() : endGame()
+}
+
+
 //Executa todas as funções criadas
 //Cria um loop que enquanto não houver vencedor e não "DEU VELHA", continuará pedindo jogadas
 //Quando uma das condições é satisfeita, temos um resultado para um vencedor e outro para empate
 //Ao final, é perguntado se os jogadores desejam jogar novamente com um leve tratamento de erro nas possiveis respostas
 //Caso os jogadores não desejem mais jogar, o placar final é impresso e o programa é finalizado.
 function ticTacToe() {
+    restartBoard(boardData)
     while (isWinner(boardData) === false && isOldLady(boardData) === false) {
         drewBoard(boardData)
         setPlayerMovement(prompt(`Jogador [ ${currentNamePlayer} ] faça sua jogada --> `))
@@ -178,19 +148,11 @@ function ticTacToe() {
         console.log(`O vencedor é: ${currentNamePlayer}`)
         scoreGame()
     }
-    else if (isOldLady(boardData) === true) {
+    else {
         console.log('____DEU VELHA____')
         scoreGame()
     }
-
-    if (continueGame() === true) {
-        restartBoard(boardData)
-        ticTacToe()
-    }
-    else {
-        endGame()
-    }
-
+    continueGame()
 
 }
 
